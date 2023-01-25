84fd850d37cb686cd29071153f28b1f9eebb4e60
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-01-25 13:11:55 -0600

    ordenamiento

---
+++ client/src/components/Sort/Sort.jsx
@@ -0,0 +1,15 @@
+
+const Sort = ({sortHandler}) => {
+    
+    return (
+        <div>
+            <select onChange={sortHandler}>
+                <option disabled defaultValue> Seleccione el orden</option>
+                <option name="A-Z">A-Z</option>
+                <option name="Z-A">Z-A</option>
+            </select>
+        </div>
+    )
+}
+
+export default Sort;
--- client/src/views/Home/Home.jsx
+++ client/src/views/Home/Home.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllDogs } from "../../redux/actions";
 import Pagination from "../../components/Pagination/Pagination"
+import Sort from "../../components/Sort/Sort";
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -16,11 +17,23 @@ const Home = () => {
     const dogs = useSelector(state => state.dogs);
     const [items, setItems] = useState([])
     const [currentPage, setCurrentPage] = useState(0);
-
-    if(dogs[0] && !items[0]){
+    let dogsSorted = [];
+    if (dogs[0] && !items[0]) {
         setItems([...dogs].splice(0, dogsPerPage));
-    };
-    
+    }
+    const sortHandler = (event) => {
+        const value = event.target.value;
+        if (value === "A-Z") {
+            dogsSorted = dogs.sort((a, b) => a.name.localeCompare(b.name));
+            setItems([]);
+            setItems([...dogsSorted].splice(0, dogsPerPage));
+        } else if (value === "Z-A") {
+            dogsSorted = dogs.sort((a, b) => b.name.localeCompare(a.name));
+            setItems([]);
+            setItems([...dogsSorted].splice(0, dogsPerPage));
+        }
+    }
+
     const prevHandler = () => {
         const prevPage = currentPage - 1;
         const firstIndex = prevPage * dogsPerPage;
@@ -33,11 +46,11 @@ const Home = () => {
         const totalDogs = dogs.length;
         const nextPage = currentPage + 1;
         const firstIndex = nextPage * dogsPerPage;
-        if (firstIndex > totalDogs) return ;
+        if (firstIndex > totalDogs) return;
         setItems([...dogs].splice(firstIndex, dogsPerPage));
         setCurrentPage(nextPage);
     }
-    
+
     return (
         <div>
             <p>Home</p>
@@ -45,6 +58,8 @@ const Home = () => {
             <Search />
             <br />
             <Pagination prevHandler={prevHandler} nextHandler={nextHandler} />
+            <br />
+            <Sort sortHandler={sortHandler} />
             <br />
             <CardsContainer dogs={items} />
             <br />
