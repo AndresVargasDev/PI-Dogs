609c9d9c4299627cea8ced6d5837a0d53a035f55
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-01-29 13:42:30 -0600

    landing page

--- client/src/views/LandingPage/LandingPage.jsx
+++ client/src/views/LandingPage/LandingPage.jsx
@@ -1,12 +1,20 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
+import style from './LandingPage.module.css'
 
 const LandingPage = () => {
     return (
-        <div>
-            <p>Landing Page</p>
-            <Link to="/home">
-                <button>Home</button>
-            </Link>
+        <div className={style.LandingPage}>
+            <title>Landing Page</title>
+            <div className={style.container}>
+                <br />
+                <h1>Dogs!</h1>
+                <br />
+                <p>En este proyecto encontraran bastante información sobre razas de perros y sus cualidades.</p>
+                <Link to="/home">
+                    <button>Entrar</button>
+                </Link>
+            </div>
         </div>
     )
 }
