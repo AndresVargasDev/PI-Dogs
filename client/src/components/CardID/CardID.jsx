75938177b9eb0e66e8a4e8175c6e1a9496cd74ad
akanashimi <113636928+akanashimi@users.noreply.github.com>
2023-02-04 20:39:43 -0600

    ajuste estilos card id

--- client/src/components/CardID/CardID.jsx
+++ client/src/components/CardID/CardID.jsx
@@ -71,51 +71,51 @@ const CardID = ({ loading }) => {
                     </div>
                 </div>
                 : null}
-            {
-                loading ? (<>
-                    {dog.length === 0 ? (
-                        <>
-                            <img className={style.img} src="https://stormgain.com/sites/default/files/news/DOGE%20breed.jpg" alt="does not exist" />
-                            <div className={style.container}>
-                                <h1 className={style.name}>The sought breed does not exist</h1>
-                                <p>From: does not exist </p>
-                                <h4 className={style.weight}>Weight</h4>
-                                <span> There is no information</span>
-                                <h4 className={style.height}>Height</h4>
-                                <span>There is no information</span>
-                                <h4 className={style.lifeSpan}>Life Span</h4>
-                                <span>There is no information</span>
-                                <h4 className={style.temperaments}>Temperament:</h4>
-                                <span>There is no information</span>
-                            </div>
-                        </>) : (<> <img className={style.img} src={dog.image} alt={dog.name} />
-                            <div className={style.container}>
-                                <h1 className={style.name}>{dog.name}</h1>
-                                <p>From: {dog.from}</p>
-                                <h4 className={style.weight}>Weight</h4>
-                                <span>{dog.minWeight} - {dog.maxWeight} kgs</span>
-                                <h4 className={style.height}>Height</h4>
-                                <span>{dog.minHeight} - {dog.maxHeight} cms</span>
-                                <h4 className={style.lifeSpan}>Life Span</h4>
-                                <span>{dog.minLifeSpan} - {dog.maxLifeSpan} years</span>
-                                <span>{dog.life_span}</span>
-                                <h4 className={style.temperaments}>Temperament:</h4>
-                                <span>{dog.temperaments}</span>
-                                {dog.from === "DataBase" ? (
-                                    <div>
-                                        <Link to={`/home/modify/${dog.id}`}>
-                                            <button className={style.buttonModify} name="Modify">Modify</button>
-                                        </Link>
-                                        <button onClick={() => clicHandler()} className={style.buttonDelete} name="Delete">Delete</button>
-                                    </div>) : null}
-                            </div>
-                        </>)}
-                </>) : (<>
-                    <img className={style.img} src="/loading.gif" alt="Loading..." />
-                    <div className={style.container}>
-                        <h1 className={style.name}>Loading...</h1>
-                    </div>
-                </>)
+            {loading ? (<>
+                {dog.length === 0 ? (
+                    <>
+                        <img className={style.img} src="https://stormgain.com/sites/default/files/news/DOGE%20breed.jpg" alt="does not exist" />
+                        <div className={style.container}>
+                            <h1 className={style.name}>The sought breed does not exist</h1>
+                            <p>From: does not exist </p>
+                            <h4 className={style.weight}>Weight</h4>
+                            <span> There is no information</span>
+                            <h4 className={style.height}>Height</h4>
+                            <span>There is no information</span>
+                            <h4 className={style.lifeSpan}>Life Span</h4>
+                            <span>There is no information</span>
+                            <h4 className={style.temperaments}>Temperament:</h4>
+                            <span>There is no information</span>
+                        </div>
+                    </>) : (<> <img className={style.img} src={dog.image} alt={dog.name} />
+                        <div className={style.container}>
+                            <h1 className={style.name}>{dog.name}</h1>
+                            <p>From: {dog.from}</p>
+                            <h4 className={style.weight}>Weight</h4>
+                            <span>{dog.minWeight} - {dog.maxWeight} kgs</span>
+                            <h4 className={style.height}>Height</h4>
+                            <span>{dog.minHeight} - {dog.maxHeight} cms</span>
+                            <h4 className={style.lifeSpan}>Life Span</h4>
+                            <span>{dog.minLifeSpan} - {dog.maxLifeSpan} years</span>
+                            <span>{dog.life_span}</span>
+                            <h4 className={style.temperaments}>Temperament:</h4>
+                            <span>{dog.temperaments}</span>
+                            <br />
+                            {dog.from === "DataBase" ? (
+                                <div className={style.modDel}>
+                                    <Link to={`/home/modify/${dog.id}`}>
+                                        <button className={style.buttonModify} name="Modify">Modify</button>
+                                    </Link>
+                                    <button onClick={() => clicHandler()} className={style.buttonDelete} name="Delete">Delete</button>
+                                </div>) : null}
+                        </div>
+                    </>)}
+            </>) : (<>
+                <img className={style.img} src="/loading.gif" alt="Loading..." />
+                <div className={style.container}>
+                    <h1 className={style.name}>Loading...</h1>
+                </div>
+            </>)
             }
         </div >
     )
